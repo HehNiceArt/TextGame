b3a923802ff0dc867bb70d05bcd64f17aac51d85
Andrei Quirante <122663831+HehNice0@users.noreply.github.com>
2023-06-06 14:32:01 +0800

    commit to main
    Followed the flowchart.

--- Assets/Scripts/Text_Game_Manager.cs
+++ Assets/Scripts/Text_Game_Manager.cs
@@ -15,13 +15,24 @@ public class Text_Game_Manager : MonoBehaviour
     public int STA_value = 10;
     public GameObject Lvl1_Choices;
     public GameObject Lvl2_Choices;
+    public GameObject Lvl3_Choices;
+    public GameObject Lvl4_Choices;
+    public GameObject Lvl5_Choices;
+    public GameObject Lvl6_Choices;
+    public GameObject MainMenu;
 
-    public float timeRemaining = 3;
-    public bool timeIsRunning = false;
+    public bool end = false;
+ 
     // Start is called before the first frame update
     void Start()
     {
+        Lvl1_Choices.SetActive(false);
         Lvl2_Choices.SetActive(false);
+        Lvl3_Choices.SetActive(false);
+        Lvl4_Choices.SetActive(false);
+        Lvl5_Choices.SetActive(false);
+        Lvl6_Choices.SetActive(false);  
+        storyText = "School Life";
     }
 
     // Update is called once per frame
@@ -30,103 +41,133 @@ public class Text_Game_Manager : MonoBehaviour
         storyTextMeshPro.text = storyText;
         HPMeshPro.text = HP_value.ToString();
         STAMeshPro.text = STA_value.ToString();
+    }
 
-
-        if (timeIsRunning)
+    public void ending()
+    {
+        if (end == true)
         {
-            if (timeRemaining > 0)
-            {
-                timeRemaining -= Time.deltaTime;
-                //Debug.Log(timeRemaining.ToString());
-            }
-            else
-            {
-                Debug.Log("No more time.");
-                timeRemaining = 0;
-                timeIsRunning = false;
-            }
+            storyText = "THE END";
         }
     }
 
-
-    public void Lvl1_openMailbox()
+    public void startGame()
     {
-        storyText = "You find a leaflet.\r\n[STA -1]";
-            STA_value -= 1;
+        Lvl1_Choices.SetActive(true);
+        MainMenu.SetActive(false);
+        storyText = "You wake up and prepared yourself to go to school. As you walk along the road heading towards the school, you encounter a black cat blocking your way. You take a moment to consider what to do with the black cat.";
+    }
+
+    public void Lvl1_petCat()
+    {
+        storyText = "As you pet the cat, you forgot the passing of time and was\r\nlocked out of the school.\r\n The end.";
+           
   
-        Debug.Log("Level 1 Open Mailbox pressed.");
+        Debug.Log("Level 1 Pet Cat pressed.");
+        Debug.Log("Level 1 choices deactivated.");
+        Lvl1_Choices.SetActive(false);
+        end = true;
     }
 
-    public void Lvl1_openDoor()
+    public void Lvl1_ignoreCat()
     {
-        storyText = "The door is boarded... You can't enter without a specific item to pry open the door.\r\n[STA -1]";
-        if (STA_value == 10)
-        {
-            STA_value -= 1;
-        }
-        Debug.Log("Level 1 Open Door pressed.");
-    }
-
-    public void Lvl1_ignore()
-    {
-        
-        storyText = "You ignored the boarded door and the mailbox.\r\nExplore the open fields?";
+        storyText = "You ignored the cat and walked to school without being\r\nlate. Sitting at the class, you...";
+        Debug.Log("Level 1 Ignore Cat pressed.");
+        Debug.Log("Level 1 choices deactivated.");
         Lvl1_Choices.SetActive(false);
         Lvl2_Choices.SetActive(true);
-
-        Debug.Log("Level 1 Ignore button pressed, Level 1 choices deactivated.");
-        Debug.Log("Level 2 choices appear.");
-
     }
 
-    public void Lvl2_ExploreFields()
+    public void Lvl2_focusClass()
     {
-        storyText = "You explored the fields and found nothing!\r\n[STA -5]";
-        Debug.Log("Level 2 Explore fields pressed");
-        Debug.Log("Start 5 second timer.");
-        
-           STA_value -= 5;
-        
+        storyText = "You focused on the class and listened attentively.\r\nThe teacher rewarded you 5 class points for listening properly. As the class finished you went to the...";
 
+        Debug.Log("Level 2 Daydream pressed.");
+        Debug.Log("Level 2 Focus Class pressed");
         Lvl2_Choices.SetActive(false);
-        timeIsRunning = true;
-        if (timeIsRunning == false)
-        {
-            storyText = "Go back?";
-            Debug.Log("Timer 0 second");
-        }
+        Lvl3_Choices.SetActive(true);
     }
 
-    public void Lvl3_GoBack()
+    public void Lvl2_daydream()
     {
-        
-       /* if (timeRemaining > 0)
-        {
-            storyText = "Go back?";
-            Debug.Log("Timer 0 second");
-        }
-       */
-    }
+        storyText = "The teacher caught you daydreaming and penalized you with -5 class points and sent you outside while raising your arms up.\r\nThe end.";
 
-    public void Lvl2_Ignore()
-    {
-        storyText = "You suddenly had a heart attack!\r\n[HP - 10]";
-        if (HP_value == 10)
-        {
-            HP_value -= 10;
-        }
-        if (STA_value >= 1)
-        {
-            STA_value = 0;
-        }
-        Debug.Log("Level 1 Button (Ignore) pressed! HP -10, STA -10");
+        Debug.Log("Level 2 Daydream pressed.");
+        Debug.Log("Level 2 choices deactivated.");
         Lvl2_Choices.SetActive(false);
-
-        if (timeRemaining == 0)
-        {
-            Application.Quit();
-        }
     }
 
+    public void Lvl3_cafeteria()
+    {
+        storyText = "You went to the cafeteria and ordered food for lunch.\r\n You feel fulla nd well rested. You go back to class.";
+        Debug.Log("Level 3 Cafeteria pressed.");
+        Debug.Log("Level 3 choices deactivated.");
+        Debug.Log("Proceed to level 4");
+        Lvl3_Choices.SetActive(false);
+        Lvl4_Choices.SetActive(true);
+    }
+
+    public void Lvl3_backyard()
+    {
+        storyText = "You went to the school backyard and...";
+        Debug.Log("Level 3 Backyard pressed.");
+        Debug.Log("Level 3 choices deactivated.");
+        Debug.Log("Proceed to level 5");
+        Lvl5_Choices.SetActive(true);
+        Lvl3_Choices.SetActive(false);
+    }
+
+    public void Lvl4_focusClass()
+    {
+        storyText = "As the class finished, you went back to your house.\r\n The end.";
+        Debug.Log("Level 4 Focus Class pressed.");
+        Debug.Log("Leve 4 choices deactivated.");
+        Lvl4_Choices.SetActive(false);
+    }
+    public void Lvl4_daydream() 
+    {
+        storyText = "The teacher caught you daydreaming and penalized you with -5 class points and sent you outside while raising your arms up.\r\n The end.";
+        Debug.Log("Level 4 Daydream pressed.");
+        Debug.Log("Leve 4 choices deactivated.");
+        Lvl4_Choices.SetActive(false);
+    }
+
+    public void Lvl5_playAround()
+    {
+        storyText = "You played around and felt exhausted. You go back to class.";
+        Debug.Log("Level 5 Play Around pressed.");
+        Debug.Log("Level 5 choices deactivated.");
+        Debug.Log("Proceed to level 6");
+        Lvl6_Choices.SetActive(true);
+        Lvl5_Choices.SetActive(false);
+    }
+    
+    public void Lvl5_explore()
+    {
+        storyText = "As you explore the backyard, you accidentally tripped on a pebble and hit the floor face first. You lost consciousness.\r\nThe end";
+        Debug.Log("Level 5 Explore pressed.");
+        Debug.Log("Level 5 choices deactivated.");
+        Lvl5_Choices.SetActive(false);
+    }
+
+    public void Lvl6_focusClass()
+    {
+        storyText = "As you felt exhausted from playing earlier, you accidentally dozed off. You got caught by the teacher and forced you to stay outside while raising your arms up until the end of class.\r\nThe end.";
+        Debug.Log("Level 6 Focus Class pressed.");
+        Debug.Log("Level6 choices deactivated.");
+        Lvl6_Choices.SetActive(false);
+    }
+
+    public void Lvl6_dozeOff()
+    {
+        storyText = "You covered your face with an open book raised vertically. The teacher didn't caught you sleeping. As the class finished, you woke up and go back to your house.\r\nThe end.";
+        Debug.Log("Level 6 Doze Off pressed.");
+        Debug.Log("Level6 choices deactivated.");
+        Lvl6_Choices.SetActive(false);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 
 }
