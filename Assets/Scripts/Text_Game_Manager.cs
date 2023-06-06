990c24d94338e2125797936887985bc389fb0163
Andrei Quirante <andrei.quirante@ciit.edu.ph>
2023-06-06 10:53:56 +0800

    commit to main
    Implemented delta.Time function

--- Assets/Scripts/Text_Game_Manager.cs
+++ Assets/Scripts/Text_Game_Manager.cs
@@ -16,10 +16,12 @@ public class Text_Game_Manager : MonoBehaviour
     public GameObject Lvl1_Choices;
     public GameObject Lvl2_Choices;
 
+    public float timeRemaining = 3;
+    public bool timeIsRunning = false;
     // Start is called before the first frame update
     void Start()
     {
-       
+        Lvl2_Choices.SetActive(false);
     }
 
     // Update is called once per frame
@@ -30,17 +32,29 @@ public class Text_Game_Manager : MonoBehaviour
         STAMeshPro.text = STA_value.ToString();
 
 
-        Lvl2_Choices.SetActive(false);
-
+        if (timeIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                //Debug.Log(timeRemaining.ToString());
+            }
+            else
+            {
+                Debug.Log("No more time.");
+                timeRemaining = 0;
+                timeIsRunning = false;
+            }
+        }
     }
+
 
     public void Lvl1_openMailbox()
     {
-        storyText = "You find a leaflet.";
-        if (STA_value == 10)
-        {
+        storyText = "You find a leaflet.\r\n[STA -1]";
             STA_value -= 1;
-        }
+  
+        Debug.Log("Level 1 Open Mailbox pressed.");
     }
 
     public void Lvl1_openDoor()
@@ -50,15 +64,52 @@ public class Text_Game_Manager : MonoBehaviour
         {
             STA_value -= 1;
         }
+        Debug.Log("Level 1 Open Door pressed.");
     }
 
     public void Lvl1_ignore()
     {
+        
         storyText = "You ignored the boarded door and the mailbox.\r\nExplore the open fields?";
-        Lvl2_Choices.SetActive(true);
         Lvl1_Choices.SetActive(false);
+        Lvl2_Choices.SetActive(true);
 
-        /*
+        Debug.Log("Level 1 Ignore button pressed, Level 1 choices deactivated.");
+        Debug.Log("Level 2 choices appear.");
+
+    }
+
+    public void Lvl2_ExploreFields()
+    {
+        storyText = "You explored the fields and found nothing!\r\n[STA -5]";
+        Debug.Log("Level 2 Explore fields pressed");
+        Debug.Log("Start 5 second timer.");
+        
+           STA_value -= 5;
+        
+
+        Lvl2_Choices.SetActive(false);
+        timeIsRunning = true;
+        if (timeIsRunning == false)
+        {
+            storyText = "Go back?";
+            Debug.Log("Timer 0 second");
+        }
+    }
+
+    public void Lvl3_GoBack()
+    {
+        
+       /* if (timeRemaining > 0)
+        {
+            storyText = "Go back?";
+            Debug.Log("Timer 0 second");
+        }
+       */
+    }
+
+    public void Lvl2_Ignore()
+    {
         storyText = "You suddenly had a heart attack!\r\n[HP - 10]";
         if (HP_value == 10)
         {
@@ -69,11 +120,13 @@ public class Text_Game_Manager : MonoBehaviour
             STA_value = 0;
         }
         Debug.Log("Level 1 Button (Ignore) pressed! HP -10, STA -10");
-        Lvl1_Choices.SetActive(false);
-        */
-    }
-    public void Lvl2_ignore()
-    {
+        Lvl2_Choices.SetActive(false);
 
+        if (timeRemaining == 0)
+        {
+            Application.Quit();
+        }
     }
+
+
 }
